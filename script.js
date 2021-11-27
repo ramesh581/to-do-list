2c3da615f8a307741887f7eca055b2eb58e753b1
ramaesh <rameshpenjerla@gmail.com>
2021-11-27 10:10:01 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function makeupper(str){
-  return str.toUpperCase();
-}
-
-function reverseString(str){
-  let splitString = str.split("");
-  let reverseArray = splitString.reverse();
-  let joinarray = reverseArray.join("");
-  return joinarray;
-}
-
-function calculate (str,callback){
-
-  return makeupper(str), reverseString(str);
-}
-
-console.log(calculate("ramesh",makeupper));
-console.log(calculate("hello",reverseString));
+console.log("hello world");

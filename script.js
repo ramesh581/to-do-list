6980e32c3abc2eb8615a59a2ebc2d254b51c049a
Ramesh Penjerla <rameshpenjerla@gmail.com>
2022-07-29 02:01:16 +0530

    updated

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ button.addEventListener("click",()=>{
     const text = document.querySelector("#task");
 
     let newList = document.createElement("li");
+    let del = document.createElement("button");
+    let text1 = document.createTextNode("done");
+    del.appendChild(text1);
     newList.innerHTML=text.value;
     let list = document.querySelector(".list");
-    list.appendChild(newList);
+    list.append(newList,del);
     text.value="";
 
-})
+});
+

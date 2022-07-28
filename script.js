ea7c3ce8da3f2c9f0b2866a265fe5b5f2c7d0fdc
Ramesh Penjerla <rameshpenjerla.com>
2022-07-28 10:02:23 +0530

    to do list

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const button = document.querySelector(".btn");
+button.addEventListener("click",()=>{
+    const text = document.querySelector("#task");
+
+    let newList = document.createElement("li");
+    newList.innerHTML=text.value;
+    let list = document.querySelector(".list");
+    list.appendChild(newList);
+    text.value="";
+
+})
